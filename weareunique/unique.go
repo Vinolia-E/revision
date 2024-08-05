7a202170ecf577a81881251540434b1f39b42dd8
vandisi <vinolia.b.19@gmail.com>
2024-08-05 14:50:20 +0300

    feat: weareunique function

--- weareunique/unique.go
+++ weareunique/unique.go
@@ -33,3 +33,46 @@ $ go run .
 2
 -1
 6*/
+
+package main
+
+import (
+	"strings"
+	"fmt"
+)
+
+func main() {
+	fmt.Println(WeAreUnique("foo", "boo"))
+	fmt.Println(WeAreUnique("", ""))
+	fmt.Println(WeAreUnique("abc", "def"))
+	fmt.Println(WeAreUnique("", "banana"))
+}
+
+func WeAreUnique(str1 , str2 string) int {
+	count := 0
+	result := ""
+	if len(str1)== 0 && len(str2) == 0 {
+		return -1
+	}
+	for _, ch := range str1 {
+		if !strings.Contains(str2, string(ch)){
+			if strings.Contains(result, string(ch)) {
+				continue
+			}
+			result += string(ch)
+			count++
+		}
+	}
+
+	for _, c := range str2 {
+		if !strings.Contains(str1, string(c)){
+			if strings.Contains(result, string(c)) {
+				continue
+			}
+			result += string(c)
+			count++
+		}
+	}
+	 fmt.Println(result)
+	return count
+}
