43f1ebe815fb58fb7eb15632d5b52b075afa8ed8
vandisi <vinolia.b.19@gmail.com>
2024-08-05 14:51:01 +0300

    feat: unzipstring function

--- unzipstring/unzip.go
+++ unzipstring/unzip.go
@@ -48,3 +48,66 @@ $Invalid Input
 $Invalid Input
 $Invalid Input
 */
+
+package main
+
+import (
+	"fmt"
+)
+
+func Unzipstring(s string) string {
+	for _, ch := range s {
+		if !Lower(ch) && !Upper(ch) && !Num(ch) {
+			return "Invalid Input"
+		}
+	}
+
+	var result string
+	for i := 0; i < len(s); i+=2 {
+
+			if Num(rune(s[i])) && Lower(rune(s[i+1])) {
+				number := int(s[i] - '0')
+				char := rune(s[i+1])
+
+				for x := 0; x < number; x++ {
+					result += string(char)
+				}
+				number = 0
+			} else if Num(rune(s[i])) && Upper(rune(s[i+1])) {
+				number := int(s[i] - '0')
+				char := rune(s[i+1])
+
+				for x := 0; x < number; x++ {
+					result += string(char)
+				}
+				number = 0
+			} else {
+				return "Invalid Input2"
+			}
+	}
+	return result
+}
+
+func main() {
+	fmt.Println(Unzipstring("2f"))
+	fmt.Println(Unzipstring("2O5u2H0e"))
+	fmt.Println(Unzipstring("3p6i1W"))
+	fmt.Println(Unzipstring("6H8a"))
+	fmt.Println(Unzipstring("2p4;7g"))
+	fmt.Println(Unzipstring("2a 6p8f"))
+	fmt.Println(Unzipstring("2t4dD"))
+	fmt.Println(Unzipstring("82t4D"))
+	fmt.Println(Unzipstring(""))
+}
+
+func Lower(r rune) bool {
+	return r >= 'a' && r <= 'z'
+}
+
+func Num(r rune) bool {
+	return r >= '0' && r <= '9'
+}
+
+func Upper(r rune) bool {
+	return r >= 'A' && r <= 'Z'
+}
